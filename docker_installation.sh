d2242ca801ca9b70a0279a7570df612c0c0e8d87
Jaswinder Singh <59972508+jassi-devops@users.noreply.github.com>
2022-07-20 23:59:08 +0530

    Update docker_installation.sh

--- docker_installation.sh
+++ docker_installation.sh
@@ -15,8 +15,8 @@ echo ""
 echo ""
 
 echo "========== Adding Docker’s official GPG key: =========="
-#sudo mkdir -p /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 sleep 5
 echo ""
